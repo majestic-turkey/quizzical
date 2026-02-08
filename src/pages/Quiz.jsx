cbccb34ebdcc4e3706aee4c4750b98b92c79660a
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-02-08 13:09:07 -0500

    add dummy trivia for formatting

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react"
+import trivia from "./trivia"
+
 export default function Quiz() {
+    const [triviaQuestions, setTriviaQuestions] = useState(trivia.results)
+
+    const triviaEls = triviaQuestions.map((question, index) => {
+        return <div key={index}>
+            <h3>{question.question}</h3>
+            <p>{question.correct_answer}</p>
+        </div>
+    })
+
     return <div className="quiz-container">
       <h1>Quiz</h1>
-      <p>Quiz content goes here...</p>
+      {triviaEls}
     </div>
 }
